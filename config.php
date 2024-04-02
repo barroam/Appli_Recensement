7b54def336a7ab835a84cc80016d6bad3795e239
meblo98 <lomouhamedelbachir@gmail.com>
2024-04-02 12:05:46 +0000

    ajout du fichier de configuration

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'BD_resencement');
+
+try {
+    // Connexion à la base de données en utilisant PDO
+    $connexion = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    
+} catch(PDOException $e) {
+    // Affichage d'un message d'erreur et arrêt du script si la connexion échoue
+    die("ERREUR: Impossible de se connecter. " . $e->getMessage());
+}
