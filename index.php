32f0d9fe6e016ddc04ccbdb01669c6527d17973f
Amadou Barro <barroama23@gmail.com>
2024-04-02 12:37:37 +0000

    Ajout du formulaire d'enregistrement

--- index.php
+++ index.php
@@ -4,10 +4,78 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recensement</title>
+     <!-- Bootstrap CSS -->
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
     
-<h1>Bienvenue</h1>
+
+
+
+<br>
+
+<div class="container">
+    <div class="row d-flex justify-content-center">
+        <div class="col_md-7"><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            <div class="card shadow">
+                <div class="card-header">
+                    <h1>Enregistrement d'un habitant</h1>
+                </div>
+                <div class="card-body">
+                    <form  method="POST" action="" enctype="multipart/form-data">
+
+                    <label for="">Matricule</label>
+                        <input type="text" name="matricule" placeholder="entrer votre nom" class="form-control">
+
+
+                        <label for="">Nom</label>
+                        <input type="text" name="nom" placeholder="entrer votre nom" class="form-control">
+
+                        <label for="">Prenom</label>
+                        <input type="text" name="prenom" placeholder="entrer votre email" class="form-control">
+
+                        <label for="">Age</label>
+                        <input type="number" name="age" placeholder="entrer votre téléphone" class="form-control">
+
+                        
+                        <label for="">Sexe</label>
+                        <input type="text" name="sexe" placeholder="entrer votre photo" class="form-control">
+
+                        <label for="">Situation Matrimonial</label>
+                        <input type="text" name="situation_matrimonial" placeholder="entrer votre email" class="form-control">
+
+                        <label for="">Status</label>
+                        <input type="text" name="status" placeholder="entrer votre email" class="form-control">
+                      
+                      <input type="submit_ajout" value="enregistrement" class="btn btn-success form-control" >
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
 </body>
